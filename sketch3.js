450374a7c3cf7c9380c03fd7ee587ab0fd45ccd7
Yu-Ying Lee <yu-yinglee@Yu-Yings-MacBook-Pro.local>
2019-11-03 11:57:12 -0500

    Initial commit

--- sketch3.js
+++ sketch3.js
@@ -1,10 +1,16 @@
 var inc = 0.01;
 
+var arrayOfPics = [];
+
 function setup() {
 	canvas = createCanvas(windowWidth,windowHeight);
 	canvas.position(0,0);
 	canvas.style('z-index', '-1');
 	pixelDensity(1);
+
+	for(var i = 0; i < 50; i++) {
+		arrayOfPics.push(new Pic());
+	}
 	
 }
 
@@ -21,4 +27,59 @@ function draw() {
 		}
 	}
 	updatePixels()
+
+	for (var i = 0; i < arrayOfPics.length; i++) {
+		arrayOfPics[i].show();
+		//arrayOfPics[i].move();
+	}
+}
+
+//image class
+class Pic {
+	constructor() {
+		this.x = random(windowWidth);
+		this.y = 0;
+		
+	}
+
+	show() {
+		fetch('https://yesno.wtf/api')
+		.then(function(results){
+			return results.json()
+		})
+		.then(function(jsonResults){
+
+
+		if(jsonResults.answer === "yes"){
+			//display();
+			return;
+		} else {
+			var containter = document.getElementById("imageRain");
+			var randoX = random(windowWidth) + 'px';
+			containter.innerHTML += `<img class="picOfNos" src="${jsonResults.image}"
+									 style="position: absolute; top: 0; left=${randoX};"
+									  >`;
+									   //style="position: absolute; top: 0; left=${randoX};"
+			 var picOfNos = document.querySelectorAll(".picOfNos");
+			 picOfNos.setAttribute("style", "position: absolute;");
+			 picOfNos.style.top = 0;
+			 picOfNos.style.left = random(windowWidth) + 'px';
+			//arrayOfPics.push(jsonResults.image);
+			//console.log(arrayOfNos);
+			//document.getElementById("myImage").src = jsonResults.image
+		} 
+	})
+
+
+	.catch(function(error){
+		console.log("error message:", error)
+	})
+
+	}
+
+	move() {
+		this.y = this.y + 10;
+
+
+	}
 }
